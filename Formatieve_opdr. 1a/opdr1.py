a2ca1d712d3c9412edd71d38430c3cee8a6a3654
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2021-02-01 10:29:47 +0100

    Update opdr1.py

--- Formatieve_opdr. 1a/opdr1.py
+++ Formatieve_opdr. 1a/opdr1.py
@@ -1,11 +1,11 @@
-size = int(input("How many blocks tall: "))
+size = int(input("How many blocks high: "))
 
 if size == 0:
     print('No pyramid for you.')
 elif size < 0:
     print('Pyramid can\'t go underground!')
 else:
-    for blocks in range(1,size+1):
+    for blocks in range(0,size):
         print (blocks * '*')
     for blocks in range(size-1, 0, -1):
         print (blocks * '*')
